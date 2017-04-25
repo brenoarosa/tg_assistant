e07edf3b092853e343b22ab6af891f74ec7df64a
Breno Arosa <breno.arosa@twistsystems.com>
2017-04-24 23:42:41 -0300

    Adds configuration file for private keys

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
+#include <libconfig.h++>
 
-int main(int argc, char *argv[]){
+using namespace std;
+using namespace libconfig;
+
+int main(int argc, char *argv[]) {
+
+    Config keys;
+    // Read the file. If there is an error, report it and exit.
+    try {
+        keys.readFile("keys.cfg");
+    }
+    catch(const FileIOException &fioex) {
+        std::cerr << "I/O error while reading file." << std::endl;
+        return 1;
+    }
+    catch(const ParseException &pex) {
+        std::cerr << "Parse error at " << pex.getFile() << ":" << pex.getLine()
+                << " - " << pex.getError() << std::endl;
+        return 2;
+    }
+
+    string id = keys.lookup("id");
+    string hash = keys.lookup("hash");
     std::cout << "Hello World!" << std::endl;
+    std::cout << "ID: " << id << "\tHash: " << hash << std::endl;
     return 0;
 }
